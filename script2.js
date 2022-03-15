deddb19f888cd6993f44d4501e349af583fc3eb3
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-03-15 16:59:50 +0000

    updated

---
+++ script2.js
@@ -0,0 +1,13 @@
+   
+    let stuff = document.querySelector("#image1");
+    stuff.addEventListener("click", toggleAction)    
+    
+ 
+    
+    
+
+
+
+let toggleAction = (event) => {
+stuff.classList.add = "you-are-hidden"
+}
